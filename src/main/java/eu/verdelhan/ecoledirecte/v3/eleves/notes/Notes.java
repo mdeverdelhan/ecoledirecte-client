1862fcb66f9827c1be840f1c5ef67e2def2f30b9
mdeverdelhan <marc.deverdelhan@yahoo.com>
2024-04-04 04:48:28 +0200

    Add comment

--- src/main/java/eu/verdelhan/ecoledirecte/v3/eleves/notes/Notes.java
+++ src/main/java/eu/verdelhan/ecoledirecte/v3/eleves/notes/Notes.java
@@ -14,7 +14,7 @@ public class Notes {
 
     @Expose
     private String foStat;
-    /** LSUN = Livret Scolaire Unique */
+    /** LSUN = Livret Scolaire Unique (cle: code de periode, ex. A001, valeur: liste des matieres) */
     @SerializedName("LSUN")
     private Map<String, List<Matiere>> lSUN;
     @Expose
